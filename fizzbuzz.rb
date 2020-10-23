52aee70d4869fc1eed1dfab3273a35bdc9299959
Chloe Atwood <atwoodksw@gmail.com>
2020-10-23 02:59:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,9 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 
+    "Fizz"
+  if int % 5 == 0 
+    "Buzz"
   end
 end
