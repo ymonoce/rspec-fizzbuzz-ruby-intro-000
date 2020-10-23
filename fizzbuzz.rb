cbb56ab09980cbd552cb0a939091b34c95fdb830
Chloe Atwood <atwoodksw@gmail.com>
2020-10-23 02:59:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 
+  if int % 3 == 0
     "Fizz"
-  if int % 5 == 0 
+  if int % 5 == 0
     "Buzz"
   end
 end
